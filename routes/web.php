10e1066a5300bfb395a9d7af5dc694ef6baacc8e
JSebastianRL <laverdexd@gmai.com>
2023-06-01 07:02:57 -0500

    scafolding

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Auth::routes();
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
-Route::get('/hola1', function () {
-    return 'holas';
-});
-Route::get('/hola2', function () {
-    return view('saludo');
-});
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
